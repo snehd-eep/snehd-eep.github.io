ec8dd7facb01a32e43a34bfa144d69b6cdd50879
snehd-eep <snehdeep240400@gmail.com>
2021-05-25 20:47:02 +0530

    home page fixed

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,5 @@ String kaboutme =
     'I started my journey as an App developer in March 2020 by learning Android App Development and after having a descent knowledge in the field I shifter to Flutter to develop beautifull cross-platform apps easily. I have done 3 complete flutter projects and did one internship.';
 
 int kIconSize = 40;
+
+var kPrimarColor = Color(0xFFFF5F1F);
--- lib/sections/homescreen.dart
+++ lib/sections/homescreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:portfolio/animation/entranceFader.dart';
-import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -11,65 +10,97 @@ class Homescreen extends StatefulWidget {
   _HomescreenState createState() => _HomescreenState();
 }
 
-class _HomescreenState extends State<Homescreen> {
+class _HomescreenState extends State<Homescreen>
+    with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    animation = Tween<double>(begin: -10, end: 0).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     Size size = MediaQuery.of(context).size;
+    var other = 0.085;
     return Container(
-      width: size.width,
-      constraints: BoxConstraints(maxHeight: size.height * 0.95),
+      height: height,
+      width: width,
       color: Colors.transparent,
       child: Stack(
         children: [
           Positioned(
-            bottom: 0,
-            right: 0,
+            bottom: animation.value,
+            right: animation.value,
             child: SvgPicture.asset(
               'assets/svgs/1.svg',
-              width: size.width / 3,
-              height: size.height / 2,
+              height: width < 1200 ? height * 0.4 : height * 0.485,
             ),
           ),
           Positioned(
-            top: 10,
-            left: 0,
+            top: 10 + animation.value,
+            left: animation.value,
             child: SvgPicture.asset(
               'assets/svgs/2.svg',
-              width: size.width / 3,
-              height: size.height / 2,
+              height: width < 1200 ? height * 0.35 : height * 0.415,
             ),
           ),
           Positioned(bottom: 100, right: 10, child: socialslist()),
           Positioned(
             left: size.width / 5,
-            top: size.height / 4,
+            top: size.height / 5,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text('Hi, I am ',
                     style: GoogleFonts.montserrat(
                         color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40)),
+                        fontSize:
+                            width < 1200 ? height * 0.055 : height * 0.065)),
                 SizedBox(
-                  height: 10,
+                  height: height * 0.04,
                 ),
                 Container(
-                    padding: EdgeInsets.all(5),
+                    padding: EdgeInsets.all(8),
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(5),
-                        color: Colors.white),
-                    child: Text('Snehdeep',
+                        border: Border.all(color: Colors.white, width: 5)),
+                    child: Text(' Snehdeep ',
                         style: GoogleFonts.montserrat(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 40))),
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400,
+                          fontSize:
+                              width < 1200 ? height * 0.085 : height * 0.095,
+                        ))),
               ],
             ),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
 
